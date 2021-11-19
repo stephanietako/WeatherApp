c0db8edb2ffb6591c5d9a5d86a188d8e111701eb
stephanietako <gabeshine@live.fr>
2021-11-19 17:03:22 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+//c'est mon appel à l'api avec la city en paramètre de fonction
+ let apiCall = function (city) {
+
+  
+  fetch(url).then((response) =>
+    response.json().then((data) => {
+      console.log(data)
+      document.querySelector('#city').innerHTML = data.name;
+      document.querySelector('#temp').innerHTML = 
+        "<i class='fas fa-temperature-high'></i>"  + " " + data.main.temp + '°';
+      document.querySelector('#humidity').innerHTML =
+        "<i class='fas fa-tint'></i>"  + " " + data.main.humidity + '%';
+      document.querySelector('#wind').innerHTML = 
+        "<i class='fas fa-wind'></i>" + " " + data.wind.speed + 'km/h';
+    })
+  )
+  .catch((err) => {
+    console.log('Error : ' + err)
+    apiCall('Honolulu');
+    alert("Sorry guys I don't have this city")
+  });
+  }
+  //c'est l'écouteur d'évenement sur la soumission du formulaire
+ document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let ville = document.querySelector('#inputCity').value;
+    apiCall(ville);
+  });
+apiCall('Honolulu');
+
+
+
+  
+
+
